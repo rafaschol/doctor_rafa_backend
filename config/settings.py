db22e8fc0a6a5b7694d35a1fa14087d105c6cb00
Rafael Schol <rafael.schol@conexiogroup.com>
2022-07-04 02:07:20 -0300

    Configured database

--- config/settings.py
+++ config/settings.py
@@ -151,3 +151,7 @@ CORS_ALLOWED_ORIGINS = [
     f"https://{os.environ['BACKEND_URL']}",
     os.environ["FRONTEND_URL"],
 ]
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES["default"].update(db_from_env)
